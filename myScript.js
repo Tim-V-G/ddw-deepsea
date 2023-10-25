0b3073d0dd19cbdedb89193ac37a9a8559d05de1
YenteWeekers <y.weekers@sintlucasedu.nl>
2023-10-25 10:28:17 +0200

    5 animals

---
+++ myScript.js
@@ -0,0 +1,30 @@
+function an1(){
+    close()
+    document.getElementById("an1-info").style.display= "flex";
+}
+
+function an2(){
+    close()
+    document.getElementById("an2-info").style.display= "flex";
+}
+
+function an3(){
+    close()
+    document.getElementById("an3-info").style.display= "block";
+}
+function an4(){
+    close()
+    document.getElementById("an4-info").style.display= "block";
+}
+function an5(){
+    close()
+    document.getElementById("an5-info").style.display= "block";
+}
+
+function close(){
+    document.getElementById("an1-info").style.display= "none";
+    document.getElementById("an2-info").style.display= "none";
+    document.getElementById("an3-info").style.display= "none";
+    document.getElementById("an4-info").style.display= "none";
+    document.getElementById("an5-info").style.display= "none";
+}
