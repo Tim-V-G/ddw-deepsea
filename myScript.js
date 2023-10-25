8f49803bdb18c82936075bad887b4ab99fef2c12
Tim <tim.van.gestel@sintlucasedu.nl>
2023-10-25 13:49:02 +0200

    extra page

--- myScript.js
+++ myScript.js
@@ -8,19 +8,27 @@ function an1(){
 function an2(){
     close()
     document.getElementById("an2-info").style.display= "flex";
+    document.getElementById("an2-info").style.justifyContent= "center";
+    document.getElementById("an2-info").style.flexDirection= "column";
 }
 
 function an3(){
     close()
-    document.getElementById("an3-info").style.display= "block";
+    document.getElementById("an3-info").style.display= "flex";
+    document.getElementById("an3-info").style.justifyContent= "center";
+    document.getElementById("an3-info").style.flexDirection= "column";
 }
 function an4(){
     close()
-    document.getElementById("an4-info").style.display= "block";
+    document.getElementById("an4-info").style.display= "flex";
+    document.getElementById("an4-info").style.justifyContent= "center";
+    document.getElementById("an4-info").style.flexDirection= "column";
 }
 function an5(){
     close()
-    document.getElementById("an5-info").style.display= "block";
+    document.getElementById("an5-info").style.display= "flex";
+    document.getElementById("an5-info").style.justifyContent= "center";
+    document.getElementById("an5-info").style.flexDirection= "column";
 }
 
 function close(){
